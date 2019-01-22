aa21e5f198b1b15f0c69df028cc5e938a66a681c
DeathScript <robochoa@est.ecotec.edu.ec>
2019-01-22 11:23:33 -0500

    [Update]: Navigation
    Navegacion entre pestañas mejorada

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Header} from 'react-native-elements'
+import {Header, Icon} from 'react-native-elements'
 import {Actions} from 'react-native-router-flux';
 
 export default class HomeScreen extends React.Component{
@@ -7,9 +7,11 @@ export default class HomeScreen extends React.Component{
     render(){
         return(
             <Header
-            leftComponent={{ icon: 'menu', color: '#fff' }}
+            //leftComponent={  }
             centerComponent={{ text: 'MY TITLE', style: { color: '#fff' } }}
-            rightComponent={{ icon: 'home', color: '#fff' }}
+            rightComponent={<Icon name="person" color='#fff' 
+            onPress={()=> Actions.Profile()}
+            ></Icon>}
           />
         );
     }
--- components/ProfileScreen.js
+++ components/ProfileScreen.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import {Header} from 'react-native-elements'
-
-
+import {Header, Button} from 'react-native-elements'
+import Icon from 'react-native-vector-icons/FontAwesome';
+import {Actions} from 'react-native-router-flux'
 export default class ProfileScreen extends React.Component{
+
+   
+
 
     render(){
         return(
             <Header
-            leftComponent={{ icon: 'menu', color: '#fff' }}
+            leftComponent={
+                <Icon name="home" size={30} color="white" onPress={()=>Actions.Home()}/>
+            }
             centerComponent={{ text: 'Profile', style: { color: '#fff' } }}
             rightComponent={{ icon: 'home', color: '#fff' }}
           />
